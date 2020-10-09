1c803be918408cc9071a78b7bd63efc39c399955
somkiet073 <somkiet073@users.noreply.github.com>
2020-10-09 21:23:34 +0700

    add middlewares

---
+++ middlewares/jwtauth.go/jwtauth.go
@@ -0,0 +1,31 @@
+package jwtauth
+
+import (
+	"golang-restful-api-using-jwt/api/accountapi"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var secretKey = "MySecret"
+
+func JWTAuth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		tokenString := r.Header.Get("key")
+		if tokenString == "" {
+			accountapi.ResponseWithError(w, http.StatusUnauthorized, "Unauthorized")
+		} else {
+			tokenString := r.Header.Get("key")
+			result, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+				return []byte(secretKey), nil
+			})
+
+			if err != nil && result.Valid {
+				next.ServeHTTP(w, r)
+			} else {
+				accountapi.ResponseWithError(w, http.StatusUnauthorized, "Unauthorized")
+			}
+			accountapi.ResponseWithJson(w, http.StatusUnauthorized, "Unauthorized")
+		}
+	})
+}
